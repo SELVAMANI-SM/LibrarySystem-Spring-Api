dccd61e18cec1ede0d4e79482d41846d0df3da85
SELVAMANI-SM <selva872000@gmail.com>
2022-05-16 22:36:20 +0530

    updated corsconfig

--- src/main/java/com/librarybooksystem/LibrarySystemApplication.java
+++ src/main/java/com/librarybooksystem/LibrarySystemApplication.java
@@ -17,7 +17,7 @@ public class LibrarySystemApplication {
 	 return new WebMvcConfigurer() {
 	   @Override
 	   public void addCorsMappings(CorsRegistry registry) {
-	     registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+	     registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
 	   }
 	 };
 	}
